599bd441c411fdce7cf8d91ec2ed05c96723be78
Alejandro <240151@fppiramide.com>
2026-02-26 11:04:30 +0100

    Registro/login done

--- src/Administrador/infrastructure/rest/Administrador.restController.ts
+++ src/Administrador/infrastructure/rest/Administrador.restController.ts
@@ -14,16 +14,20 @@ const administradorUseCases: AdministradorUseCases = new AdministradorUseCases(
 const router = express.Router();
 
 router.post("/registro", async (request: Request, response: Response) => {
-    
+
     const { alias, correo, passwrd } = request.body;
-    
+
     const AdminPost = {
         alias,
         correo,
         passwrd
     }
     const administrador: Administrador = await administradorUseCases.registro(AdminPost);
-    response.send(administrador)
+    const token = createToken(administrador);
+    response.status(200).send({
+        message: "Registro correcto",
+        token: token
+    })
 })
 
 router.post("/login", async (request: Request, response: Response) => {
@@ -35,19 +39,19 @@ router.post("/login", async (request: Request, response: Response) => {
         passwrd
     }
 
-    const administrador: Administrador |false  = await administradorUseCases.login(loginAdmin);
-    console.log(administrador,"Controller");
+    const administrador: Administrador | false = await administradorUseCases.login(loginAdmin);
+    console.log(administrador, "Controller");
     if (!administrador) {
         response.status(400).send({
             message: "Admin no encontrado",
             token: null
         })
-    }else if(administrador.passwrd === null){
+    } else if (administrador.passwrd === null) {
         response.status(400).send({
             message: "Contraseña incorrecte",
             token: null
         })
-    }else{
+    } else {
         const token = createToken(administrador);
         response.status(200).send({
             message: "Credenciales correctas",
