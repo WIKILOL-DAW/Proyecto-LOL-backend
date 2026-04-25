fb862cf1738367242bf58fbf0842a00f85079ca6
Adrián Rami <230053@fppiramide.com>
2026-04-25 17:43:46 +0200

    entidad noticia teminada, creando a mano las noticias en la BD

---
+++ src/Noticia/infraestructure/rest/Noticia.restController.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import express, { request, Request, Response } from "express";
+import NoticiaRepository from "../../domain/Noticia.repository";
+import NoticiaPostgresSQL from '../db/Noticia.repositoryPostgresSQL';
+import NoticiaUseCases from "../../application/Noticia.useCase";
+
+
+const noticiaRepository: NoticiaRepository = new NoticiaPostgresSQL();
+const noticiaUseCases: NoticiaUseCases = new NoticiaUseCases(noticiaRepository)
+const router = express.Router();
+
+router.get(`/verNoticias`,async (request: Request,response:Response)=>{
+    try{
+        const noticias = await noticiaUseCases.cargarNoticias();
+
+         response.status(200).json({
+            noticias
+        });
+    }catch(error){
+          console.log("ERROR: ", error);
+        response.status(500).json({
+            message: "Error al mostrar las Noticias"
+        });
+    }
+})
