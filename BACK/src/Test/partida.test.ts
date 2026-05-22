3508f06127821eb2059a8d73eb4394ecaec657cc
Alex <240151@fppiramide.com>
2026-05-22 16:36:17 +0200

    Test

--- BACK/src/Test/partida.test.ts
+++ BACK/src/Test/partida.test.ts
@@ -21,3 +21,38 @@ describe("GET /obtenerPartidas", () => {
     });
 
 });
+
+describe("POST /nuevaPartida", () => {
+
+    it("deberia crear una nueva partida", async () => {
+
+        const nuevaPartida = {
+
+            fechaPartida: "2025-06-01T18:00:00",
+            equipoRojo: "G2 Esports",
+            equipoAzul: "Fnatic",
+            equipoGanador: "G2 Esports",
+            killsEquipoAzul: 8,
+            killsEquipoRojo: 25,
+            torneo: "LEC",
+            split: "SPRING",
+            año: 2025,
+            fase: "regular"
+        };
+
+        const response = await request(app)
+            .post("/api/partida/nuevaPartida")
+            .send(nuevaPartida);
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toBeDefined();
+
+        expect(response.body.partida)
+            .toBeDefined();
+
+        expect(response.body.partida.equipoRojo)
+            .toBe("G2 Esports");
+    });
+
+});
