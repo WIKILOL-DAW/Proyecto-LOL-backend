106ef651f77099bcf0fc846682e610eb2e04bf4c
Adrianramipiramide <152392777+Adrianramipiramide@users.noreply.github.com>
2026-05-26 12:35:08 +0200

    Update server.ts

--- BACK/src/server.ts
+++ BACK/src/server.ts
@@ -7,8 +7,9 @@ import app from "./index";
 //});
 
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, "0.0.0.0", () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
